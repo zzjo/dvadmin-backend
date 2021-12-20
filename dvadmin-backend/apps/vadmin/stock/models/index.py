921516cebced38a504f71cab0e0f29778e345015
zzjo <814829740@qq.com>
2021-12-20 17:41:22 +0800

    指数

---
+++ dvadmin-backend/apps/vadmin/stock/models/index.py
@@ -0,0 +1,17 @@
+from django.db.models import CharField, DecimalField
+
+from apps.vadmin.op_drf.models import CoreModel
+
+
+class Index(CoreModel):
+    code = CharField(max_length=64, verbose_name="代码")
+    name = CharField(max_length=64, verbose_name="名称")
+    latest_price = DecimalField(max_digits=19, decimal_places=4, verbose_name="最新价")
+    change_percent = DecimalField(max_digits=19, decimal_places=3, verbose_name="涨跌额")
+    price_change = DecimalField(max_digits=19, decimal_places=3, verbose_name="涨跌幅")
+    closed = DecimalField(max_digits=19, decimal_places=4, verbose_name="昨收")
+    open = DecimalField(max_digits=19, decimal_places=4, verbose_name="今开")
+    height = DecimalField(max_digits=19, decimal_places=4, verbose_name="最高")
+    low = DecimalField(max_digits=19, decimal_places=4, verbose_name="最低")
+    volume = DecimalField(max_digits=19, decimal_places=4, verbose_name="成交量")
+    amount = DecimalField(max_digits=19, decimal_places=4, verbose_name="成交额")
